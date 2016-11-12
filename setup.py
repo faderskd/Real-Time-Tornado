96780b8214b420e9a57705fa1a46df6240f1ce80
daniel.faderski <daniel.faderski@jmr.pl>
2016-11-12 16:13:02 +0100

    Setup configuration file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='realtime-tornado',
+setup(name='realtime_tornado',
       version='0.1',
       description='Websocket handling with tornado',
       url='https://github.com/faderskd/Real-Time-Tornado',
       author='Daniel Fąderski',
       author_email='daniel.faderski@gmail.com',
       license='MIT',
-      packages=['realtime-tornado'],
+      packages=['realtime_tornado'],
       install_requires=[
         'tornado==4.4.2',
         'redis==2.10.5'
